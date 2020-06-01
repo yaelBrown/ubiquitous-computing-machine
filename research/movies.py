8220e67daaf821dca3d3671cdf402216d660c59b
Yael Brown <yaelrbrown@gmail.com>
2020-06-01 08:16:20 -0500

    able to get title and movie url

--- research/movies.py
+++ research/movies.py
@@ -1,6 +1,23 @@
 import json
 
-if __name__ == "__main__":
-  # Read secrets file
-  with open('secrets.json') as secrets:
-    data = json.load(secrets)
+movies = []
+
+with open("data.html", "r+") as f:
+  lines = f.readlines()
+  for line in lines:
+    temp = {}
+    if '<a class="yt-simple-endpoint style-scope ytd-playlist-video-renderer" href="' in line:
+      url = line[84:line.find('">')]
+    if '<span id="video-title"' in line:
+      title = line[97:line.find(" by Y")]
+      temp["url"] = url
+      temp["title"] = title
+      del url
+    if bool(temp) == True: 
+      movies.append(temp)
+
+with open("movies.json", "w") as outfile: 
+  json.dump(movies, outfile)
+
+
+# print(movies)
